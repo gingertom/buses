430c413e1e5282b4db9ffa7ad1715e7fd1a2de11
Tom Melamed <tom@gingertom.co.uk>
2019-05-30 13:06:33 +0100

    Starting to look at patterns.

--- pipeline/bournemouth_input/data_reader.py
+++ pipeline/bournemouth_input/data_reader.py
@@ -20,15 +20,6 @@ stop_events = pd.merge(
     right_on=["workid", "date"],
 )
 
-# Find every row that has a valid previous stop on the same
-# pattern with the same vehicle on the same date
-stop_events["match"] = np.logical_and(
-    np.logical_and(
-        stop_events.date == stop_events.date.shift(1),
-        stop_events.vehicle == stop_events.vehicle.shift(1),
-    ),
-    stop_events.patternId == stop_events.patternId.shift(1),
-)
 
 # Convert aimed arrival and departure to datetime opbjects.
 stop_events["aimedArrival"] = pd.to_datetime(
@@ -39,14 +30,41 @@ stop_events["aimedDeparture"] = pd.to_datetime(
     stop_events["date"] + pd.to_timedelta(stop_events["aimedDeparture"], unit="s")
 ).astype("datetime64[ns]")
 
-# Generate new columns with the details of the previous stop, it
-# doesn't matter if this isn't valid as we'll only export the valid ones
-stop_events["prev_stopCode"] = stop_events.stopCode.shift(1)
-stop_events["prev_aimedArrival"] = stop_events.aimedArrival.shift(1)
-stop_events["prev_aimedDeparture"] = stop_events.aimedDeparture.shift(1)
-stop_events["prev_actualArrival"] = stop_events.actualArrival.shift(1)
-stop_events["prev_actualDeparture"] = stop_events.actualDeparture.shift(1)
+# Load the patterns we'll need this to make sure that each bus stop is recorded in order
+patterns = pd.read_csv("Trapeze_Data/Patterns.csv")
 
-print(stop_events.iloc[3000:3003])
+# Group by data and id (which collectively is a unique identifier for a unit of work)
+# and patternId, we don't strictly need to group by patternId but it's a handy way of getting it out
+single_patterns = stop_events.groupby(["date", "id", "patternId"])
 
-stop_events[stop_events.match is True].to_csv("stop_events.csv", index=False)
+for name, group in single_patterns:
+    print(name[2])
+
+    for row_index, row in group.iterrows():
+        print(row)
+
+        # For each row see if the one above it or the one below it matches the previous stop code
+        # Remember that some patterns are cyclical and so we can't just start from the top and
+        # find the first stop code that matches
+
+# # Find every row that has a valid previous stop on the same
+# # pattern with the same vehicle on the same date
+# stop_events["match"] = np.logical_and(
+#     np.logical_and(
+#         stop_events.date == stop_events.date.shift(1),
+#         stop_events.vehicle == stop_events.vehicle.shift(1),
+#     ),
+#     stop_events.patternId == stop_events.patternId.shift(1),
+# )
+
+# # Generate new columns with the details of the previous stop, it
+# # doesn't matter if this isn't valid as we'll only export the valid ones
+# stop_events["prev_stopCode"] = stop_events.stopCode.shift(1)
+# stop_events["prev_aimedArrival"] = stop_events.aimedArrival.shift(1)
+# stop_events["prev_aimedDeparture"] = stop_events.aimedDeparture.shift(1)
+# stop_events["prev_actualArrival"] = stop_events.actualArrival.shift(1)
+# stop_events["prev_actualDeparture"] = stop_events.actualDeparture.shift(1)
+
+# print(stop_events.iloc[3000:3003])
+
+# stop_events[stop_events.match is True].to_csv("stop_events.csv", index=False)
