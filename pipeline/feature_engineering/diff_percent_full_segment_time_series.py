e86ba626a22556710cece26313f1ea10d92b7e44
Tom Melamed <tom@gingertom.co.uk>
2019-06-26 21:13:01 +0100

    Using train test split and feather file format.

--- pipeline/feature_engineering/diff_percent_full_segment_time_series.py
+++ pipeline/feature_engineering/diff_percent_full_segment_time_series.py
@@ -6,6 +6,8 @@ from argparse import ArgumentParser
 import os.path
 from pathlib import Path
 
+import feather
+
 
 def pivot(stop_events):
 
@@ -17,42 +19,42 @@ def pivot(stop_events):
         stop_events["arrival_hour"].values, unit="h"
     )
 
-    pivoted_5mins_code_hour_day = stop_events.pivot_table(
+    pivoted_5mins_code_hour_day = stop_events[stop_events["train"]].pivot_table(
         index="arrival_5mins",
         columns="segment_code",
         values="diff_percent_full_segment_and_median_by_segment_code_and_hour_and_day",
         aggfunc=np.mean,
     )
 
-    pivoted_5mins_code = stop_events.pivot_table(
+    pivoted_5mins_code = stop_events[stop_events["train"]].pivot_table(
         index="arrival_5mins",
         columns="segment_code",
         values="diff_percent_full_segment_and_median_by_segment_code",
         aggfunc=np.mean,
     )
 
-    pivoted_10mins_code_hour_day = stop_events.pivot_table(
+    pivoted_10mins_code_hour_day = stop_events[stop_events["train"]].pivot_table(
         index="arrival_10mins",
         columns="segment_code",
         values="diff_percent_full_segment_and_median_by_segment_code_and_hour_and_day",
         aggfunc=np.mean,
     )
 
-    pivoted_10mins_code = stop_events.pivot_table(
+    pivoted_10mins_code = stop_events[stop_events["train"]].pivot_table(
         index="arrival_10mins",
         columns="segment_code",
         values="diff_percent_full_segment_and_median_by_segment_code",
         aggfunc=np.mean,
     )
 
-    pivoted_1hour_code_hour_day = stop_events.pivot_table(
+    pivoted_1hour_code_hour_day = stop_events[stop_events["train"]].pivot_table(
         index="arrival_1hour",
         columns="segment_code",
         values="diff_percent_full_segment_and_median_by_segment_code_and_hour_and_day",
         aggfunc=np.mean,
     )
 
-    pivoted_1hour_code = stop_events.pivot_table(
+    pivoted_1hour_code = stop_events[stop_events["train"]].pivot_table(
         index="arrival_1hour",
         columns="segment_code",
         values="diff_percent_full_segment_and_median_by_segment_code",
@@ -102,34 +104,10 @@ if __name__ == "__main__":
 
     print("Loading data...")
     # Load in the stop_events from the previous stage in the pipeline
-    stop_events = pd.read_csv(args.input_filename, parse_dates=[1, 5, 6, 18, 19])
-
-    # Force it to treat all the times as actual times.
-    stop_events["aimedArrival"] = stop_events["aimedArrival"].astype("datetime64[ns]")
-    stop_events["aimedDeparture"] = stop_events["aimedDeparture"].astype(
-        "datetime64[ns]"
-    )
-    stop_events["prev_aimedArrival"] = stop_events["prev_aimedArrival"].astype(
-        "datetime64[ns]"
-    )
-    stop_events["prev_aimedDeparture"] = stop_events["prev_aimedDeparture"].astype(
-        "datetime64[ns]"
-    )
-    stop_events["prev_actualArrival"] = stop_events["prev_actualArrival"].astype(
-        "datetime64[ns]"
-    )
-    stop_events["prev_actualDeparture"] = stop_events["prev_actualDeparture"].astype(
-        "datetime64[ns]"
-    )
-
-    stop_events["date"] = stop_events["date"].astype("datetime64[ns]")
+    stop_events = feather.read_dataframe(args.input_filename)
+    stop_events = stop_events.set_index("index")
 
     print("\tLoaded")
-
-    print("Dropping nans...")
-    # Drop any rows with nan or empty sections.
-    stop_events = stop_events.dropna(axis=0)
-    print("\tDropped")
 
     pivoted_5mins_code_hour_day, pivoted_5mins_code, pivoted_10mins_code_hour_day, pivoted_10mins_code, pivoted_1hour_code_hour_day, pivoted_1hour_code = pivot(
         stop_events
@@ -144,39 +122,45 @@ if __name__ == "__main__":
 
     path_5mins.mkdir(parents=True, exist_ok=True)
 
-    pivoted_5mins_code_hour_day.to_csv(
+    pivoted_5mins_code_hour_day = pivoted_5mins_code_hour_day.reset_index()
+    pivoted_5mins_code_hour_day.to_feather(
         str(path_5mins)
-        + "/diff_percent_from_code_hour_day_-_full_segment_time_series.csv"
+        + "/diff_percent_from_code_hour_day_-_full_segment_time_series.feather"
     )
 
-    pivoted_5mins_code.to_csv(
-        str(path_5mins) + "/diff_percent_from_code_-_full_segment_time_series.csv"
+    pivoted_5mins_code = pivoted_5mins_code.reset_index()
+    pivoted_5mins_code.to_feather(
+        str(path_5mins) + "/diff_percent_from_code_-_full_segment_time_series.feather"
     )
 
     path_10mins = to_write_path / Path("10mins")
 
     path_10mins.mkdir(parents=True, exist_ok=True)
 
-    pivoted_10mins_code_hour_day.to_csv(
+    pivoted_10mins_code_hour_day = pivoted_10mins_code_hour_day.reset_index()
+    pivoted_10mins_code_hour_day.to_feather(
         str(path_10mins)
-        + "/diff_percent_from_code_hour_day_-_full_segment_time_series.csv"
+        + "/diff_percent_from_code_hour_day_-_full_segment_time_series.feather"
     )
 
-    pivoted_10mins_code.to_csv(
-        str(path_10mins) + "/diff_percent_from_code_-_full_segment_time_series.csv"
+    pivoted_10mins_code = pivoted_10mins_code.reset_index()
+    pivoted_10mins_code.to_feather(
+        str(path_10mins) + "/diff_percent_from_code_-_full_segment_time_series.feather"
     )
 
     path_1hour = to_write_path / Path("1hour")
 
     path_1hour.mkdir(parents=True, exist_ok=True)
 
-    pivoted_1hour_code_hour_day.to_csv(
+    pivoted_1hour_code_hour_day = pivoted_1hour_code_hour_day.reset_index()
+    pivoted_1hour_code_hour_day.to_feather(
         str(path_1hour)
-        + "/diff_percent_from_code_hour_day_-_full_segment_time_series.csv"
+        + "/diff_percent_from_code_hour_day_-_full_segment_time_series.feather"
     )
 
-    pivoted_1hour_code.to_csv(
-        str(path_1hour) + "/diff_percent_from_code_-_full_segment_time_series.csv"
+    pivoted_1hour_code = pivoted_1hour_code.reset_index()
+    pivoted_1hour_code.to_feather(
+        str(path_1hour) + "/diff_percent_from_code_-_full_segment_time_series.feather"
     )
 
     print("\tWritten")
