e29c2ab0734bd0951ded87ca5570bbc35fa4c671
Tom Melamed <tom@gingertom.co.uk>
2019-06-26 21:23:21 +0100

    Lots of updates to the not_luigi script to match everything else that has been added.

--- not_luigi.sh
+++ not_luigi.sh
@@ -1,24 +1,70 @@
 #!/bin/bash
 
-# /Users/tommelamed/anaconda3/envs/busses/bin/python "pipeline/bournemouth_input/data_reader.py"
+PYT=/Users/tommelamed/anaconda3/envs/busses/bin/python
 
-# /Users/tommelamed/anaconda3/envs/busses/bin/python "pipeline/feature_engineering/add_geo_features.py" -i "data_files/B/stop_events.csv" -o "data_files/B/unfiltered/stop_events_with_geo.csv"
+# print_progress() {
+#     echo "###############################"
+#     echo "$1"
+#     echo "###############################"
+# }
 
-/Users/tommelamed/anaconda3/envs/busses/bin/python "pipeline/feature_engineering/filter_rare_and_overtakes.py" -i "data_files/B/unfiltered/stop_events_with_geo.csv" -once "data_files/B/once/stop_events_with_geo.csv" -twice "data_files/B/twice/stop_events_with_geo.csv"
+# if [ ! -f "data_files/B/stop_events.csv" ]; then
 
-/Users/tommelamed/anaconda3/envs/busses/bin/python "pipeline/feature_engineering/add_features.py" -i "data_files/B/once/stop_events_with_geo.csv" -o "data_files/B/once/stop_events_with_geo_averages.csv" 
+#     print_progress "Starting Data Reader"
+#     $PYT "pipeline/bournemouth_input/data_reader.py"
+# fi
 
-/Users/tommelamed/anaconda3/envs/busses/bin/python "pipeline/feature_engineering/add_features.py" -i "data_files/B/twice/stop_events_with_geo.csv" -o "data_files/B/twice/stop_events_with_geo_averages.csv" 
+# if [ ! -f "data_files/B/unfiltered/stop_events_with_geo.csv" ]; then
 
-# /Users/tommelamed/anaconda3/envs/busses/bin/python "pipeline/feature_engineering/add_features.py" -i "data_files/B/unfiltered/stop_events_with_geo.csv" -o "data_files/B/unfiltered/stop_events_with_geo_averages.csv" 
+#     print_progress "Starting Geo Features"
 
-/Users/tommelamed/anaconda3/envs/busses/bin/python "pipeline/feature_engineering/diff_percent_full_segment_time_series.py" -i "data_files/B/once/stop_events_with_geo_averages.csv" 
+#     $PYT "pipeline/feature_engineering/add_geo_features.py" \
+#         -i "data_files/B/stop_events.csv" \
+#         -o "data_files/B/unfiltered/stop_events_with_geo.csv"
+# fi
 
-/Users/tommelamed/anaconda3/envs/busses/bin/python "pipeline/feature_engineering/diff_percent_full_segment_time_series.py" -i "data_files/B/twice/stop_events_with_geo_averages.csv" 
+# if [ ! -f "data_files/B/once/stop_events_with_geo.csv" ] ; then
 
-# /Users/tommelamed/anaconda3/envs/busses/bin/python "pipeline/feature_engineering/diff_percent_full_segment_time_series.py" -i "data_files/B/unfiltered/stop_events_with_geo_averages.csv" 
+#     print_progress "Starting Rare and overtakes"
+
+#     $PYT "pipeline/feature_engineering/filter_rare_and_overtakes.py" \
+#         -i "data_files/B/unfiltered/stop_events_with_geo.csv" \
+#         -once "data_files/B/once/stop_events_with_geo.csv" 
+# fi
+
+# if [ ! -f "data_files/B/once/35days/stop_events_with_geo_train_test.feather" ] ; then
+
+#     print_progress "Starting Train Test Split (once)"
+
+#     $PYT "pipeline/feature_engineering/train_validate_test.py" \
+#         -i "data_files/B/once/stop_events_with_geo.csv" 
+# fi
+
+# if [ ! -f "data_files/B/unfiltered/35days/stop_events_with_geo_train_test.feather" ] ; then
+
+#     print_progress "Starting Train Test Split (unfiltered)"
+
+#     $PYT "pipeline/feature_engineering/train_validate_test.py" \
+#         -i "data_files/B/unfiltered/stop_events_with_geo.csv" 
+# fi
+
+# # Find all the train test files:
+# find . -name "stop_events_with_geo_train_test.feather" \
+#     -exec $PYT \
+#     "pipeline/feature_engineering/add_features.py" -i {} \
+#     \;
+
+# # Find all the averages files:
+# find . -name "stop_events_with_geo_train_test_averages.feather" \
+#     -exec $PYT \
+#     "pipeline/feature_engineering/diff_percent_full_segment_time_series.py" -i {} \
+#     \;
 
 # Find all the time series files:
-find . -name "diff_percent_from_code_-_full_segment_time_series.csv" -exec /Users/tommelamed/anaconda3/envs/busses/bin/python "pipeline/Spatiotemporal_filters/correlations.py" -i {} \;
+find . -name "diff_percent_from_code_-_full_segment_time_series.feather" \
+    -exec $PYT \
+    "pipeline/Spatiotemporal_filters/correlations.py" -i {} \;
 
-find . -name "diff_percent_from_code_hour_day_-_full_segment_time_series.csv" -exec /Users/tommelamed/anaconda3/envs/busses/bin/python "pipeline/Spatiotemporal_filters/correlations.py" -i {} \;
+find . -name "diff_percent_from_code_hour_day_-_full_segment_time_series.feather" \
+    -exec $PYT \
+    "pipeline/Spatiotemporal_filters/correlations.py" -i {} \;
