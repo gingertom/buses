391920b2f3202377030b152d6c4d7dbf2e8f9132
Tom Melamed <tom@gingertom.co.uk>
2019-07-23 13:09:57 +0100

    Adding more offsets No longer adding rain (moved to add features) Fixed info leak with padding Can now choose interpolated (padded) or not

--- pipeline/feature_engineering/add_offsets.py
+++ pipeline/feature_engineering/add_offsets.py
@@ -11,25 +11,6 @@ import os.path
 from pathlib import Path
 
 
-def filter_rare(stop_events):
-
-    print("Calculating rare segments...")
-
-    segment_counts = stop_events.groupby("segment_code").size()
-
-    filtered_stop_events = stop_events.drop(
-        stop_events[
-            stop_events["segment_code"].isin(
-                segment_counts[segment_counts < 120].index.values
-            )
-        ].index
-    )
-
-    print("\tCalculated")
-
-    return filtered_stop_events
-
-
 def add_offsets_inner(row, ts):
 
     try:
@@ -37,12 +18,13 @@ def add_offsets_inner(row, ts):
             return np.nan
 
         value = ts.at[row[0], row[1]]
+        # value = ts[row[1]].get(row[0], np.nan)
     except KeyError:
         value = np.nan
     return value
 
 
-def add_offsets(se):
+def add_offsets(se, interpolate=True):
 
     print("Adding Offsets...")
 
@@ -51,60 +33,36 @@ def add_offsets(se):
     se["offset_timestamp_5_2"] = se["arrival_5mins"] - pd.Timedelta("10 min")
     se["offset_timestamp_5_3"] = se["arrival_5mins"] - pd.Timedelta("15 min")
     se["offset_timestamp_5_4"] = se["arrival_5mins"] - pd.Timedelta("20 min")
+    se["offset_timestamp_5_5"] = se["arrival_5mins"] - pd.Timedelta("25 min")
+    se["offset_timestamp_5_6"] = se["arrival_5mins"] - pd.Timedelta("30 min")
+    se["offset_timestamp_5_7"] = se["arrival_5mins"] - pd.Timedelta("35 min")
 
     # We need to generate this from scratch as we need both test and train data.
     ts_5 = se.pivot_table(
         index="arrival_5mins",
         columns="segment_code",
-        values="diff_percent_full_segment_and_median_by_segment_code_and_hour_and_day",
+        values="diff_percent_segment_and_median_by_segment_code_and_hour_and_day",
         aggfunc=np.median,
     )
 
-    ts_interp_5 = ts_5.interpolate(method="time", axis=0)
-
-    weather = pd.read_csv("weather/CDO9610867945337_weather.csv")
-
-    weather["date"] = pd.to_datetime(weather[" YEARMODA"], format="%Y%m%d")
-    weather["rain"] = weather["PRCP  "].apply(lambda x: float(x[:-1]))
-
-    just_rain = weather[["date", "rain"]]
-
-    se = se.merge(just_rain, left_on="date", right_on="date")
-
-    se["diff_segment_and_median_by_segment_code"] = (
-        se["segment_duration"] - se["median_durations_by_segment_code"]
-    )
-    se["diff_segment_and_median_by_segment_code_and_hour_and_day"] = (
-        se["segment_duration"] - se["median_durations_by_segment_code_and_hour_and_day"]
-    )
-
-    se["diff_percent_segment_and_median_by_segment_code"] = (
-        se["diff_segment_and_median_by_segment_code"]
-        * 100
-        / se["median_durations_by_segment_code"]
-    )
-
-    se["diff_percent_segment_and_median_by_segment_code_and_hour_and_day"] = (
-        se["diff_segment_and_median_by_segment_code_and_hour_and_day"]
-        * 100
-        / se["median_durations_by_segment_code_and_hour_and_day"]
-    )
+    if interpolate:
+        ts_5 = ts_5.interpolate(method="pad", axis=0)
 
     segment_names = ["segment_code"]
     column_names = ["self_offset"]
 
-    for i in range(1, 5):
+    for i in range(1, 8):
         segment_names.append(f"prev_segment_code_{i}")
         segment_names.append(f"next_segment_code_{i}")
         column_names.append(f"prev_stop_{i}_offset")
         column_names.append(f"next_stop_{i}_offset")
 
-    for i in range(1, 5):
+    for i in range(1, 8):
         for j in range(len(segment_names)):
-            print(".", end="")
+            print(".", end="", flush=True)
             se[f"{column_names[j]}_5_{i}"] = se[
                 [f"offset_timestamp_5_{i}", segment_names[j]]
-            ].apply(add_offsets_inner, axis=1, args=(ts_interp_5,))
+            ].apply(add_offsets_inner, axis=1, args=(ts_5,))
 
     return se
 
@@ -129,13 +87,20 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "-c",
-        dest="correlation_filename",
+        "-int",
+        dest="input_interpolate",
         required=True,
-        help="input feather file from a previous step",
-        metavar="FILE",
-        type=lambda x: is_valid_file(parser, x),
+        help="input Should we interpolate",
     )
+
+    # parser.add_argument(
+    #     "-c",
+    #     dest="correlation_filename",
+    #     required=True,
+    #     help="input feather file from a previous step",
+    #     metavar="FILE",
+    #     type=lambda x: is_valid_file(parser, x),
+    # )
 
     args = parser.parse_args()
 
@@ -157,8 +122,8 @@ if __name__ == "__main__":
         + stop_events.prev_timingPoint.str[0]
     )
 
-    correlations = feather.read_dataframe(args.correlation_filename)
-    correlations = correlations.set_index(correlations.columns[0])
+    # correlations = feather.read_dataframe(args.correlation_filename)
+    # correlations = correlations.set_index(correlations.columns[0])
 
     # stop_events = stop_events.merge(
     #     correlations["0"].to_frame(),
@@ -169,17 +134,26 @@ if __name__ == "__main__":
 
     print("\tLoaded")
 
-    stop_events = filter_rare(stop_events)
+    if args.input_interpolate == "interpolate":
+        interpolate = True
+    else:
+        interpolate = False
 
-    stop_events = add_offsets(stop_events)
+    stop_events = add_offsets(stop_events, interpolate)
 
     print("Writing output file...")
 
     stop_events = stop_events.reset_index()
 
-    stop_events.to_feather(
-        str(from_path.parent)
-        + "/stop_events_with_geo_train_test_averages_prev_next_offsets.feather"
-    )
+    if interpolate:
+        stop_events.to_feather(
+            str(from_path.parent)
+            + "/stop_events_with_geo_train_test_averages_prev_next_offsets_interp.feather"
+        )
+    else:
+        stop_events.to_feather(
+            str(from_path.parent)
+            + "/stop_events_with_geo_train_test_averages_prev_next_offsets_noninterp.feather"
+        )
 
     print("\tWritten")
